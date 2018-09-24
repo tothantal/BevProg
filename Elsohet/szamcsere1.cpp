8f1c88f4fd9f6e08f8f8559d558fd2cf495616cf
tothantal <43417284+tothantal@users.noreply.github.com>
2018-09-24 17:59:47 +0200

    Add files via upload

---
+++ Elsohet/szamcsere1.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+//változók cseréje különbséggel
+
+int main()
+{
+    int a, b;
+    cout << " a = ";
+    cin >> a;
+    cout << " b = ";
+    cin >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "a = " << a << " b = " << b;
+}
